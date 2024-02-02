9f8c7dd93de1f09e04f28cf201f5ab171b57ef19
charkster <31560568+charkster@users.noreply.github.com>
2024-02-01 21:11:14 -0700

    Add files via upload

---
+++ test_bk8500.py
@@ -0,0 +1,8 @@
+from bk8500 import bk8500
+import time
+bk8500 = bk8500(com_port='COM38',debug=True)
+bk8500.enable_remote()
+bk8500.enable_cc_mode()
+bk8500.set_cc_mode_current(current=0.15)
+bk8500.enable_input()
+bk8500.disable_input()
